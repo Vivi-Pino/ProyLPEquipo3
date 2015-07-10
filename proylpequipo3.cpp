fa582dd256911a489f448a943a1fc5d2c26923af
Angel Gualpa <mesiaspxdx@hotmai.es>
2015-07-10 08:42:19 -0500

    menu
    menu

--- proylpequipo3.cpp
+++ proylpequipo3.cpp
@@ -5,7 +5,22 @@
 
 main()
 {
+ int m;
+   printf("***Menu Principal***\n");
+   printf("1- Ingresar la nueva hora  de clases: \n");
+   printf("2- Ver el horario de hoy: \n");
+   printf("3- Ver el horario de la Semana: \n");
+   printf("4-Salir: \n");
+   scanf("%d",&m);
+   if(m>=1 && m<=4)
+   {
+   printf("menu");
 
+   }
+   else
+   {
+   printf("el dato ingresado es incorrecto");
+   }
 
 getch();
 }
