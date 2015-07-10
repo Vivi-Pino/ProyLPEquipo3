4c8887a293beb76d0c4e08bbbbdda12809e30e2d
Danilo-Alvarez <danilo.alvarezt@espoch.edu.ec>
2015-07-10 15:24:15 -0500

    menu
    switch con las opciones del menu

--- proylpequipo3.cpp
+++ proylpequipo3.cpp
@@ -5,22 +5,54 @@
 
 main()
 {
- int m;
-   printf("***Menu Principal***\n");
-   printf("1- Ingresar la nueva hora  de clases: \n");
+   int m;
+   do
+   {
+
+   printf("\n\tMENU PRINCIPAL\n");
+   printf("1- Ingresar una nueva hora de clases: \n");
    printf("2- Ver el horario de hoy: \n");
    printf("3- Ver el horario de la Semana: \n");
-   printf("4-Salir: \n");
+   printf("4- Salir. \n");
+   	do
+      {
+   printf("\nINGRESE LA OPCION A EJECUTAR: ");
    scanf("%d",&m);
-   if(m>=1 && m<=4)
-   {
-   printf("menu");
+   }while(m<=0 || m>4);
 
-   }
-   else
+
+   switch(m)
    {
-   printf("el dato ingresado es incorrecto");
+    	case 1:
+      	char mat[10], sem[10], ini[10], fin[10], dia[10];
+
+      	printf("\nIngrese los siguientes datos: \n\n");
+      	printf("NOMBRE DE LA MATERIA: \n");
+      	scanf("%s",&mat);
+      	printf("NOMBRE DEL SEMESTRE: \n");
+      	scanf("%s",&sem);
+      	printf("DIA DE LA SEMANA: \n");
+      	scanf("%s",&dia);
+      	printf("HORA DE INICIO: \n");
+      	scanf("%s",&ini);
+      	printf("HORA DE FIN: \n");
+      	scanf("%s",&fin);
+
+      break;
+
+      case 2:
+      	printf("\n\t HORARIO DE HOY \n");
+      break;
+
+      case 3:
+      	printf("\n\t HORARIO DE LA SEMANA \n");
+      break;
+
+      case 4:
+      	return 0;
+      break;
    }
+   }while(m=4);
 
 getch();
 }
