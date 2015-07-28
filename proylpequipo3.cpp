51d4668489fc16fcbf6adee83b18d11f0638556a
Vivi-Pino <vivi.pino1997@gmail.com>
2015-07-28 18:35:05 -0500

    hola

--- proylpequipo3.cpp
+++ proylpequipo3.cpp
@@ -55,18 +55,43 @@ main()
    {
 
    printf("El dato Ingresado es incorrecto");
-   char mat[10], sem[10],  dia[10], ini[10], fin[10], caracter;
+   char mat[10], sem[10],  dia[10], ini[10], fin[10];
     	case 1:
 
       	printf("\nIngrese los siguientes Datos: \n\n");
       	printf("NOMBRE DE LA MATERIA: \n");
       	scanf("%s",&mat);
+
       	printf("NOMBRE DEL SEMESTRE: \n");
       	scanf("%s",&sem);
+
       	printf("DIA DE LA SEMANA: \n");
-      	scanf("%s",&dia);
+         scanf("%s",&dia);
+
+         while(strcmp(dia,"LUN")!=0 && strcmp(dia,"MAR")!=0 && strcmp(dia,"MIE")!=0 && strcmp(dia,"JUE")!=0 && strcmp(dia,"VIE")!=0)
+         {
+          printf("Ingresos permitidos: LUN, MAR, MIE, JUE, VIE\nDIA DE LA SEMANA: \n");
+          scanf("%s",&dia);
+         }
+
       	printf("HORA DE INICIO: \n");
       	scanf("%s",&ini);
+         while(strcmp(ini,"07h00")!=0 && strcmp(ini,"08h00")!=0 && strcmp(ini,"09h00")!=0 && strcmp(ini,"10h00")!=0 && strcmp(ini,"11h00")!=0 && strcmp(ini,"12h00")!=0 && strcmp(ini,"13h00")!=0 && strcmp(ini,"14h00")!=0 && strcmp(ini,"15h00")!=0 && strcmp(ini,"16h00")!=0 && strcmp(ini,"17h00")!=0 && strcmp(ini,"18h00")!=0 && strcmp(ini,"19h00")!=0 && strcmp(ini,"20h00")!=0 && strcmp(ini,"21h00")!=0)
+         {
+          printf("Formato permitido: EJ. 07h00 \nHORA DE INICIO: \n");
+          scanf("%s",&ini);
+         }
+
+         printf("HORA DE FIN: \n");
+      	scanf("%s",&fin);
+         while(strcmp(fin,"07h00")!=0 && strcmp(fin,"08h00")!=0 && strcmp(fin,"09h00")!=0 && strcmp(fin,"10h00")!=0 && strcmp(fin,"11h00")!=0 && strcmp(fin,"12h00")!=0 && strcmp(fin,"13h00")!=0 && strcmp(fin,"14h00")!=0 && strcmp(fin,"15h00")!=0 && strcmp(fin,"16h00")!=0 && strcmp(fin,"17h00")!=0 && strcmp(fin,"18h00")!=0 && strcmp(fin,"19h00")!=0 && strcmp(fin,"20h00")!=0 && strcmp(fin,"21h00")!=0)
+         {
+          printf("Formato permitido: EJ. 07h00 \nHORA DE FIN: \n");
+          scanf("%s",&fin);
+         }
+
+
+
          printf("Presione una tecla para regresar al menu principal\n\n");
          getch();
       break;
